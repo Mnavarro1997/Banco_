8adbcb16f8f855acb7299a082bae8f0773fad15c
MNavarro1997 <mnavarro@gteam.es>
2021-09-02 13:00:03 +0200

    Agregar archivos de proyecto.

---
+++ Banco_/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banco_
+{
+    public class Person
+    {
+        public string Name;
+        public int Age;
+
+
+    }
+}
---
+++ Banco_/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banco_
+{
+   public class User
+    {
+
+        public string UserName { get; set; }
+        public string Mail { get; set; }
+
+    }
+}
